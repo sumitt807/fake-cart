577710aae7b31dc24065c9d1f737feae8909b2a7
sumitt806 <sumitt806@gmail.com>
2022-03-31 01:04:40 +0530

    Hello

--- express-tutorial/main.js
+++ express-tutorial/main.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set('views', 'public/views');
+
 
 app.get('/', function (req, res) {
-    res.send("hello this is testing");
+    res.render('index');
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.resolve(__dirname) + "/view/about.html");
 });
 
 const PORT = process.env.PORT || 3000;
